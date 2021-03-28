b328bdc6792867904da617944782d65d7233a6b9
Adaobi Aniuchi <adamichelllle@gmail.com>
2021-03-28 15:11:21 +0100

    Add prediction function

--- app.js
+++ app.js
@@ -33,12 +33,36 @@ app.use('/', express.static(path.join(__dirname + 'public')));
 });
 
 /**
- * App route. Render the app page with the search form.
+ * App route. Render the app page with the query form.
  * @param req
  * @param res
  */
  router.get('/', (req, res) => {
     res.render('pages/app')
+});
+
+/**
+ * App route. Render the app page with the search form.
+ * @param req
+ * @param res
+ */
+ router.post('/predict', (req, res) => {
+    console.log(req.body)
+    const url = "https://hbe-model-api.herokuapp.com/predict";
+
+    axios.post(url, req.body)
+    .then((response) => {
+        console.log(response);
+        res.status(200);
+        res.render('pages/predict', {
+            userInfo: req.body,
+            data: response.data.data
+        });
+    }, (error) => {
+        res.status(404).render('pages/error404', {
+            reason: "API Error"
+        });
+    });
 });
 
 /**
